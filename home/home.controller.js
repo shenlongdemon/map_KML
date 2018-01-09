8af3f5c127b39f58120df5e379bdac39eb0c203e
Long Nguyen Thanh <long.nguyenthanh@axonactive.com>
2018-01-09 16:27:45 +0700

    new

---
+++ home/home.controller.js
@@ -0,0 +1,27 @@
+﻿(function () {
+    'use strict';
+    angular
+            .module('app')
+            .controller('HomeController', HomeController);
+    HomeController.$inject = ['$rootScope', '$scope'];
+    function HomeController($rootScope, $scope) {      
+        $rootScope.title = "shenlong-webutl";
+        $scope.initMap = initMap;
+        $scope.map = {};
+        initController();
+        function initController() {
+            
+        }
+        function initMap(map){
+            $scope.map = map;
+            var georssLayer = new google.maps.KmlLayer({
+                url: 'https://raw.githubusercontent.com/mapbox/Simple-KML/master/sample/example.kml?ver=' + Date.now()  
+              });
+            georssLayer.setMap($scope.map);
+            georssLayer.addListener('click', function(kmlEvent) {
+                alert("");
+            });
+        }
+    }
+
+})();
